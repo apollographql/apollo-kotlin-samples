cd71e700db2a5b58be68687a9b7abf0cd7eca8b2
BoD <BoD@JRAF.org>
2024-10-04 11:16:37 +0200

    Avoid bidirectional = true

--- multi-modules-and-services/feature2/build.gradle.kts
+++ multi-modules-and-services/feature2/build.gradle.kts
@@ -15,29 +15,15 @@ apollo {
         srcDir("src/main/graphql/servicea")
         packageName.set("com.example.servicea")
 
-        /*
-         * Depend on the schema and fragments from service "service-a" in module "graphqlShared".
-         * 
-         * The `bidirectional` parameter allows to reduce the size of the generated code by telling
-         * upstream modules to only generate the used types.
-         *
-         * See https://www.apollographql.com/docs/kotlin/advanced/multi-modules/#auto-detection-of-used-types
-         */
-        dependsOn(project(":graphqlShared"), bidirectional = true)
+        // Depend on the schema and fragments from service "service-a" in module "graphqlShared".
+        dependsOn(project(":graphqlShared"))
     }
 
     service("service-b") {
         srcDir("src/main/graphql/serviceb")
         packageName.set("com.example.serviceb")
 
-        /*
-         * Depend on the schema and fragments from service "service-b" in module "graphqlShared".
-         * 
-         * The `bidirectional` parameter allows to reduce the size of the generated code by telling
-         * upstream modules to only generate the used types.
-         *
-         * See https://www.apollographql.com/docs/kotlin/advanced/multi-modules/#auto-detection-of-used-types
-         */
-        dependsOn(project(":graphqlShared"), bidirectional = true)
+        // Depend on the schema and fragments from service "service-b" in module "graphqlShared".
+        dependsOn(project(":graphqlShared"))
     }
 }
