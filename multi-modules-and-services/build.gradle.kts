1ed659ec6a25b283f2082241cd571de20ce07236
BoD <BoD@JRAF.org>
2026-02-12 12:36:29 +0100

    Bump AK to v5

--- multi-modules-and-services/build.gradle.kts
+++ multi-modules-and-services/build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    id("com.apollographql.apollo") version "4.3.1" apply false
+    kotlin("jvm") version "2.3.10"
+    id("com.apollographql.apollo") version "5.0.0-alpha.4" apply false
 }
