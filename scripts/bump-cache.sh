333a66a8c89216b3d6f20af04eed073139c0bfff
Benoit 'BoD' Lubek <BoD@JRAF.org>
2026-07-17 11:36:09 +0200

    Bump dependencies

--- scripts/bump-cache.sh
+++ scripts/bump-cache.sh
@@ -1,1 +1,16 @@
-find . -name "*.kts" -type f -exec perl -pi -e 's/1\.0\.0-beta\.3/1.0.0-beta.4/g' {} +
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <dependency> <X.Y.Z>"
+  echo "Example: $0 apolloCache 1.0.8"
+  exit 1
+fi
+
+dependency="$1"
+version="$2"
+
+find . -name 'libs.versions.toml' \
+  -exec sed -E -i '' \
+    "s/^(${dependency}[[:space:]]*=[[:space:]]*\").*(\")$/\1${version}\2/" \
+    {} +
